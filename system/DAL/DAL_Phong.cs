6e5eba1dc2b1050ebff5fcb327e11def89580586
tkngan2104 <tkngan2104@gmail.com>
2025-02-27 01:18:18 +0700

    doi ten class

--- system/DAL/DAL_Phong.cs
+++ system/DAL/DAL_Phong.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BUS
+namespace DAL
 {
-    public class Class1
+    public class DAL_Phong
     {
     }
 }
--- system/ET/ET_Phong.cs
+++ system/ET/ET_Phong.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace ET
 {
-    public class Class1
+    public class ET_Phong
     {
     }
 }
