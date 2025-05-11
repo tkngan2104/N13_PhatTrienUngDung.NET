401fe3e79d0fb39f930051b531a2c4f2c477d5a3
lamvietngoc <lamvietngoc93@gmail.com>
2025-05-11 09:40:17 +0700

    CRUD_2ChucNang

---
+++ system/BUS/BUS_ChiTietDatTiec.cs
@@ -0,0 +1,43 @@
+﻿using DAL;
+using ET;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BUS_ChiTietDatTiec
+    {
+        private readonly DAL_ChiTietDatTiec dal = new DAL_ChiTietDatTiec();
+        public IQueryable layDSCTDT()
+        {
+            return dal.layDSCTDT();
+        }
+
+        // Thêm chi tiết đặt tiệc
+        public bool themChiTietDatTiec(ET_ChiTiecDatTiec et)
+        {
+            return dal.themChiTietDatTiec(et);
+        }
+
+        // Xoá chi tiết đặt tiệc
+        public bool xoaChiTietDatTiec(string maCTDT)
+        {
+            return dal.XoaChiTietDatTiec(maCTDT);
+        }
+
+        // Sửa chi tiết đặt tiệc
+        public bool suaChiTietDatTiec(ET_ChiTiecDatTiec et)
+        {
+            return dal.suaChiTietDatTiec(et);
+        }
+
+        public string taoMaCTDT()
+        {
+            return dal.TaoMaCTDT();
+        }
+    }
+}
