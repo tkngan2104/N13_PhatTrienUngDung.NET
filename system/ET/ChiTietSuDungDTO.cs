98917738d08b9ffa5321160b614c81b9d8c1f6cc
lamvietngoc <lamvietngoc93@gmail.com>
2025-05-14 22:45:28 +0700

    .

---
+++ system/ET/ChiTietSuDungDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class ChiTietSuDungDTO
+    {
+        public string Ten { get; set; }       
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal ThanhTien => SoLuong * DonGia;
+        public string Loai { get; set; }    
+    }
+}
